8b70efb36dd685413c9f4e936d0ed105900547c2
Matthew Jones <matthewjones00@me.com>
2017-06-27 21:57:05 -0700

    add working form to page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,19 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @post = Post.new
   end
+
+  def create
+    puts post_params
+    Post.create(post_params)
+    @post = Post.new
+    @posts = Post.all
+    render action: "index"
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :author, :description)
+    end
 end
