6e5ed483823384c9680b7bbf56003ad33a729ac7
Matthew Jones <matthewjones00@me.com>
2017-06-28 22:55:33 -0700

    update post index css

---
+++ app/assets/javascripts/posts.js
@@ -0,0 +1,9 @@
+// $(document).ready(function() {
+//   $(".newsfeedHeadline").mouseover(function() {
+//     $(".newsfeedHeadline").css("font-weight", "600");
+//   });
+//
+//   $(".newsfeedHeadline").mouseout(function() {
+//     $(".newsfeedHeadline").css("font-weight", "100");
+//   });
+// })
